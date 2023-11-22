d5f8416d692745c2e0a180446cec0b91003fda09
Menolo-Homobovanez <118155256+Menolo-Homobovanez@users.noreply.github.com>
2023-11-21 20:04:32 -0500

    changed how all methods work, added test class

--- src/Persons.java
+++ src/Persons.java
@@ -1,4 +1,7 @@
+import java.util.Calendar;
+
 public class Persons {
+    final int CURRENTYEAR = 2023;
     String firstName;
     String lastName;
     boolean isMale;
@@ -11,22 +14,22 @@ public class Persons {
         yearOfBirth = 1950;
     }
 
-    public void setFirstName(String firstName) throws Exception {
+    public void setFirstName(String firstName){
         isNull(firstName);
         this.firstName = firstName;
     }
 
-    public void setLastName(String lastName) throws Exception {
+    public void setLastName(String lastName){
         isNull(lastName);
         this.lastName = lastName;
     }
 
-    public void setMale(boolean isMale) throws Exception {
+    public void setMale(boolean isMale){
         isNull(firstName);
         this.isMale = isMale;
     }
 
-    public void setYearOfBirth(short yearOfBirth) throws Exception {
+    public void setYearOfBirth(short yearOfBirth){
         isNull(yearOfBirth);
         this.yearOfBirth = yearOfBirth;
     }
@@ -47,11 +50,14 @@ public class Persons {
         return yearOfBirth;
     }
 
-    private void isNull(Object o) throws Exception {
+    private void isNull(Object o){
         if (o == null) {
-            throw new Exception("May not set values to null");
+            throw new Error("May not set values to null");
         }
     }
+
+    private void isTextless;
+    private void isTimefull;
 
     /**
      * Returns a string representation of the object.
@@ -60,32 +66,51 @@ public class Persons {
      */
     @Override
     public String toString() {
-        if (firstName == null) {
-            firstName = "";
-        }
+//        if (firstName == null) {
+//            firstName = "";
+//        }
 
-        return "Name is " + firstName + lastName + "\n The statment that this person is male is: " + isMale + "\n";
+        String gender = isMale ? "Man" : "Woman";
+
+        return firstName + " " + lastName + "\n is a " + (CURRENTYEAR - yearOfBirth) + " year old " + gender + "\n";
     } // should provide an annotated display of Person info
 
     @Override
     public boolean equals(Object o) {
+        if(o == null)
+            return false;
+        //TODO test of same class
+        if (!o.getClass().equals(this.getClass()))
+            return false;
 
         return o.hashCode() == this.hashCode();
     }
 
     @Override
     public int hashCode() {
-        int hashVal = 0;
-        for (int i = 0; i < firstName.length(); i++) {
-            hashVal += (int) firstName.charAt(i);
-        }
-        for (int i = 0; i < lastName.length(); i++) {
-            hashVal += (int) firstName.charAt(i);
-        }
-        hashVal += yearOfBirth;
+        //uses Godel's method for encoding of multiple values to capture three fields. best for encoding a small number of data fields.
+        //gender is captured via parity encoding. good for encoding one field.
+        int hashVal;
 
-        hashVal = isMale ? hashVal * 10 + 1 : hashVal * 10;
+        hashVal = (int) Math.pow(2, nameHasher(firstName));
+
+        hashVal *= (int) Math.pow(3, nameHasher(firstName));
+
+        hashVal *= (int) Math.pow(5, yearOfBirth);
+
+        //parity now reveals gender
+        hashVal = isMale ? hashVal * 2 + 1 : hashVal * 2;
 
         return hashVal;
     } // hand-code this method without using Objects static methods
+
+    private int nameHasher(String name){
+        int hashedBrown = 0;
+
+        for (int i = 0; i < name.length(); i++) {
+             hashedBrown += (int) name.charAt(i);
+        }
+
+        return hashedBrown;
+    }
 }
---
+++ tests/PersonsTests.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests of all supported methods of Persons.
+ */
+class PersonsTest {
+    Persons DavidChaimelwitz;
+    @BeforeEach
+    void setUp() throws Exception {
+        DavidChaimelwitz.setMale(true);
+        DavidChaimelwitz.setFirstName("David");
+        DavidChaimelwitz.
+
+    }
+
+    @Test
+    void constructorTester(){
+
+    }
+}
